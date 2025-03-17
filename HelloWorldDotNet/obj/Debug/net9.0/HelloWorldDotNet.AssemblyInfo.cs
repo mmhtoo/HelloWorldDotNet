aed7d707be6d39892df92f025f1b6dc7a3641815
Myo Min Htoo <mmhtoo.dev@gmail.com>
2025-03-17 22:43:52 +0630

    config: update docker

--- HelloWorldDotNet/obj/Debug/net9.0/HelloWorldDotNet.AssemblyInfo.cs
+++ HelloWorldDotNet/obj/Debug/net9.0/HelloWorldDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4c0e8436fec0bdef89c51e93795c0a14b6df01d")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
